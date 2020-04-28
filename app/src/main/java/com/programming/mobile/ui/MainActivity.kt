0bfa8b0fd3f197f9eca4c5eafe2914ad6923d3a4
invitejay <sksskdi122345@gmail.com>
2020-04-28 16:51:16 +0900

    Gwon - 첫 커밋

---
+++ app/src/main/java/com/programming/mobile/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.programming.mobile.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.programming.mobile.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
